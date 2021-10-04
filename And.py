a241924987a76c72290266ec8468d0ba23a9009e
41deepak <41deepak@gmail.com>
2021-10-04 16:18:13 +0530

    code organised

--- And.py
+++ And.py
@@ -3,22 +3,27 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, filename, plotname):
+    df_and = pd.DataFrame(data)
+    print(df_and)
 
-AND = {
+    X,y = prepare_data(df_and)
+    
+    model_and = Perceptron(eta=eta,  epochs=epochs)
+    model_and.fit(X,y)
+    _ = model_and.total_loss()   #Dummy variable
+
+    save_model(model_and, filename=filename)
+    save_plot(df_and, plotname, model_and)
+
+
+if __name__ == "__main__":     #entry point
+    AND = {
     "x1":[0,0,1,1],
     "x2":[0,1,0,1],
     "y":[0,0,0,1],}
 
-df_and = pd.DataFrame(AND)
-print(df_and)
+    ETA = 0.3       # 0 to 1
+    EPOCHS = 10
 
-X,y = prepare_data(df_and)
-ETA = 0.3       # 0 to 1
-EPOCHS = 10
-model_and = Perceptron( eta = ETA, epochs= EPOCHS)
-model_and.fit(X,y)
-
-_ = model_and.total_loss()
-
-save_model(model_and, filename="And.model")
-save_plot( df_and, file_name="And.png", model= model_and)
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="And.model", plotname="And.png")
--- OR.py
+++ OR.py
@@ -3,22 +3,27 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, filename, plotname):
+    df_OR = pd.DataFrame(data)
+    print(df_OR)
 
-OR = {
+    X,y = prepare_data(df_OR)
+    
+    model_OR = Perceptron(eta=eta,  epochs=epochs)
+    model_OR.fit(X,y)
+    _ = model_OR.total_loss()   #Dummy variable
+
+    save_model(model_OR, filename=filename)
+    save_plot(df_OR, plotname, model_OR)
+
+
+if __name__ == "__main__":     #entry point
+    OR = {
     "x1":[0,0,1,1],
     "x2":[0,1,0,1],
     "y":[0,1,1,1],}
 
-df_OR = pd.DataFrame(OR)
-print(df_OR)
+    ETA = 0.3       # 0 to 1
+    EPOCHS = 10
 
-X,y = prepare_data(df_OR)
-ETA = 0.3       # 0 to 1
-EPOCHS = 10
-model_OR = Perceptron( eta = ETA, epochs= EPOCHS)
-model_OR.fit(X,y)
-
-_ = model_OR.total_loss()
-
-save_model(model_OR, filename="OR.model")
-save_plot( df_OR, file_name="OR.png", model= model_OR)
+    main(data=OR, eta=ETA, epochs=EPOCHS, filename="OR.model", plotname="OR.png")
